c67d3fdf90c1a62787db3a2c24233bc9276c1693
Leonid Rastorguev <rastorguyevl@list.ru>
2022-06-22 23:20:14 +0300

    Add backend "remote"

--- versions.tf
+++ versions.tf
@@ -5,4 +5,15 @@ terraform {
       version = "0.61.0"
     }
   }
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "ra-leonid"
+
+    workspaces {
+      name = "terraform-cloud"
+      #prefix = "my-app-"
+    }
+  }
+
+  #required_version = ">= 0.14.0"
 }
