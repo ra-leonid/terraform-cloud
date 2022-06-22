4fbd003eec3afa5158f8b4d6993617b9ad77e949
Leonid Rastorguev <rastorguyevl@list.ru>
2022-06-22 23:47:59 +0300

    test

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 # Provider
-provider "registry.terraform.io/yandex-cloud/yandex" {
+provider "yandex" {
   # Only one of token or service_account_key_file must be specified.
   token     = var.yc_token
+  #service_account_key_file = var.yc_key_file
   cloud_id  = var.yc_cloud_id
   zone      = var.yc_region
 
